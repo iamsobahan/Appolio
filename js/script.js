368ac445c577cadfe5253b52077fa417c6397642
sobahan-webhunter <sobahan.webhunter@gmail.com>
2021-06-05 17:11:24 +0600

    hi

---
+++ js/script.js
@@ -0,0 +1,95 @@
+$('.app-slider').slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
+  var rev = $('.rev_slider');
+rev.on('init', function(event, slick, currentSlide) {
+  var
+    cur = $(slick.$slides[slick.currentSlide]),
+    next = cur.next(),
+    next2 = cur.next().next(),
+    prev = cur.prev(),
+    prev2 = cur.prev().prev();
+  prev.addClass('slick-sprev');
+  next.addClass('slick-snext');  
+  prev2.addClass('slick-sprev2');
+  next2.addClass('slick-snext2');  
+  cur.removeClass('slick-snext').removeClass('slick-sprev').removeClass('slick-snext2').removeClass('slick-sprev2');
+  slick.$prev = prev;
+  slick.$next = next;
+}).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+  console.log('beforeChange');
+  var
+    cur = $(slick.$slides[nextSlide]);
+  console.log(slick.$prev, slick.$next);
+  slick.$prev.removeClass('slick-sprev');
+  slick.$next.removeClass('slick-snext');
+  slick.$prev.prev().removeClass('slick-sprev2');
+  slick.$next.next().removeClass('slick-snext2');
+  next = cur.next(),  
+  prev = cur.prev();
+  //prev2.prev().prev();
+  //next2.next().next();
+  prev.addClass('slick-sprev');
+  next.addClass('slick-snext');
+  prev.prev().addClass('slick-sprev2');
+  next.next().addClass('slick-snext2');
+  slick.$prev = prev;
+  slick.$next = next;
+  cur.removeClass('slick-next').removeClass('slick-sprev').removeClass('slick-next2').removeClass('slick-sprev2');
+});
+
+rev.slick({
+  speed: 1000,
+  arrows: true,
+  dots: false,
+  focusOnSelect: true,
+  prevArrow: '<i class="fas fa-long-arrow-alt-left left"></i>',
+  nextArrow: '<i class="fas fa-long-arrow-alt-right right"></i>',
+  infinite: true,
+  centerMode: true,
+  slidesPerRow: 1,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  centerPadding: '0',
+  swipe: true,
+  customPaging: function(slider, i) {
+    return '';
+  },
+  /*infinite: false,*/
+});
